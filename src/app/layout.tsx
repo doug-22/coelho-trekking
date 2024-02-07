aefea2c476e60e54425bcc914f3d1987c2029070
Douglas <douglas.aguiar410@gmail.com>
2024-02-07 18:00:42 -0300

    feat/add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Header } from '@/components/Header';
 import StyledComponentsRegistry from '@/lib/registry';
+import { Footer } from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,10 +15,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          // charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+      </head>
       <StyledComponentsRegistry>
         <body>
           <Header />
           {children}
+          <Footer />
         </body>
       </StyledComponentsRegistry>
     </html>
